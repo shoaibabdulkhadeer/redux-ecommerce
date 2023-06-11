112e90344a5fc7fea43fd290c993369aa121c1df
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-06-11 16:17:10 +0530

    remove product from cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,89 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import {remove} from "../features/cart"
+import { useDispatch } from 'react-redux'
 
 const Cart = () => {
  
   const cart = useSelector(state => state.cart)
+ 
+  const dispatch = useDispatch()
+
+  const remBtn = (id) => {
+    dispatch(remove(id))
+  }
 
   return (
-    <div style={{minHeight: "100vh",marginTop:"80px"}}>
-         {cart.map((x) => {
+    <div style={{minHeight: "100vh",marginTop:"80px" , display:"flex",justifyContent:"center"}} className='container-fluid '>
+     <div class="container text-center">
+ 
+     
+
+        <h3 className='font-monospace mt-3'>All Products</h3>
+
+
+        {cart.map((x) => {
           return (
-            <div>
-              <h1>{x.title}</h1>
+       
+            <div class="row align-items-center justify-content-center my-3 " >
+              <div class="col-12 col-sm-3 col-lg-2 py-3">
+                   <img src={x.image} alt='' width={70}/>
+              </div>
+              <div class="col-12 col-sm-3 col-lg-2">
+                <p>{x.title} </p>
+              </div>
+
+              <div class="col-12 col-sm-3 col-lg-2">
+                 <b>{x.category}</b>
+              </div>
+
+              <div class="col-6 col-sm-3 col-lg-2">
+                 <b>Price: ${x.price}</b>
+              </div>
+            
+          
+              <div class="col-6 col-sm-3 col-lg-2">
+                 <button onClick={() => remBtn(x.id)} className='btn btn-warning'>Remove</button>
+              </div>
+              <hr className='mt-2'/>
             </div>
+           
           )
-         })}
+        })}
+                
+            
+                <div class="row align-items-center justify-content-center my-3 " >
+              <div class="col-12 col-sm-3 col-lg-2 py-3">
+                   <p style={{fontFamily:"cursive"}}>All Products</p>
+              </div>
+              <div class="col-12 col-sm-3 col-lg-2">
+                <p>{""}</p>
+              </div>
+
+              <div class="col-12 col-sm-3 col-lg-2">
+                 <b>{""}</b>
+              </div>
+
+              <div class="col-6 col-sm-3 col-lg-2">
+                 <b>Total Price:</b>
+              </div>
+            
+          
+              <div class="col-6 col-sm-3 col-lg-2">
+                 <button className='btn btn-warning'>Remove</button>
+              </div>
+              <hr className='mt-2'/>
+            </div>
+
+
+ 
+
+
+           </div>
+
+
+
+        
         </div>
   )
 }
