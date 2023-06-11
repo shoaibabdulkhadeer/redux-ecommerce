232fdddb8fc6a8aafcdff58c2b9217f80d8080e9
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-06-11 12:33:14 +0530

    minor

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,11 +10,13 @@ const Main = () => {
     <div >
       <div className="container col-xxl-10 px-4 py-5" >
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div className="col-10 col-sm-8 col-lg-6">
+
+      <div className="col-10 col-sm-8 col-lg-4">
         <img src="https://onpointfresh.com/wp-content/uploads/2017/04/Mens-Summer-Fashion..png" 
-        className="d-block mx-4 img-fluid rounded shadow" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+        className="d-block mx-4 img-fluid rounded shadow" alt="Bootstrap Themes" width="900" height="600" loading="lazy" />
       </div>
-      <div className="col-lg-6">
+
+      <div className="col-lg-8">
         <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
         <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
@@ -22,6 +24,7 @@ const Main = () => {
           <button type="button" className="btn btn-outline-secondary btn-lg px-4" fdprocessedid="93pflo">Default</button>
         </div>
       </div>
+
     </div>
   </div>
 
@@ -29,24 +32,26 @@ const Main = () => {
   
   <div className="container px-4 py-5" id="featured-3">
     <h2 className="pb-2 border-bottom">Columns with icons</h2>
-    <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-      <div className="feature col">
+
+    <div className="row g-4 py-5 ps-lg-5 row-cols-1 row-cols-lg-3">
+
+      <div className="feature col-lg-4">
         <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
            <BsClockFill size={50} style={{padding:"5px"}}/>
         </div>
         <h3 className="fs-2 text-body-emphasis">Featured title</h3>
-        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-       
+        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>       
       </div>
-      <div className="feature col">
+
+      <div className="feature col-lg-4">
         <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
          <FaAddressCard size={50} style={{padding:"5px"}}/>
         </div>
         <h3 className="fs-2 text-body-emphasis">Featured title</h3>
         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-       
       </div>
-      <div className="feature col">
+
+      <div className="feature col-lg-4">
         <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
           <AiFillAmazonSquare size={50} style={{padding:"5px"}}/>
         </div>
