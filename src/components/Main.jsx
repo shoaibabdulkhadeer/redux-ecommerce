7b29a2c8a29d212153b8859ffc25adc7313b0483
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-06-11 16:34:56 +0530

    total price reduce mehtod

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,7 +17,7 @@ const Main = () => {
       </div>
 
       <div className="col-lg-8">
-        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
+        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">The Style Club.</h1>
         <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
           <button type="button" className="btn btn-primary btn-lg px-4 me-md-2" fdprocessedid="47pw37">Primary</button>
